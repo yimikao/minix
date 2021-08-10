41d2ab861c8006323a2fd4624ab1d9e72b6e9868
yinka <holayinkajr@gmail.com>
2021-08-10 19:59:02 +0100

    pushed files

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    //Database Params
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_NAME','mvcframework');
+
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT
+    define('URLROOT', 'http://localhost/mvcwebframework/pages/index');
+
+    //Sitename
+    define('SITENAME', 'mvcwebframework');
